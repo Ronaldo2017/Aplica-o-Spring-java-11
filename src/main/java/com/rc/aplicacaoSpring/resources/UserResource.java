fb7397789ce8aaa8dfd6e152736df58f39794e70
Ronaldo2017 <Ronaldo2017@users.noreply.github.com>
2022-03-23 10:24:40 -0300

    Criado camada de serviço, e registrando um componente com @Servicena classe UserService>

--- src/main/java/com/rc/aplicacaoSpring/resources/UserResource.java
+++ src/main/java/com/rc/aplicacaoSpring/resources/UserResource.java
@@ -1,22 +1,41 @@
 package com.rc.aplicacaoSpring.resources;
 
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rc.aplicacaoSpring.entities.User;
+import com.rc.aplicacaoSpring.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+//	UserResource depende da UserService
+	
+	@Autowired
+	private UserService service;
+	
 	//end point para acesso aos usuarios
 	//responde a requisição do tipo get do http
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Pedro", "pedro@email.com", "74598437", "2458");
-		return ResponseEntity.ok().body(u);//retorna o usuario
+	public ResponseEntity<List<User>> findAll(){
+		//User u = new User(1L, "Pedro", "pedro@email.com", "74598437", "2458");
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);//retorna o usuario
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
---
+++ src/main/java/com/rc/aplicacaoSpring/services/UserService.java
@@ -0,0 +1,29 @@
+package com.rc.aplicacaoSpring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rc.aplicacaoSpring.entities.User;
+import com.rc.aplicacaoSpring.repositories.UserRepository;
+
+//registrar como componente do spring
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
