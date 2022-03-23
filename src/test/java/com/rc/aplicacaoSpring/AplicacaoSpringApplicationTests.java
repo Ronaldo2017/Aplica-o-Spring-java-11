78e18b5a5341f9d1642045788005f300a4a71f43
Ronaldo2017 <Ronaldo2017@users.noreply.github.com>
2022-03-23 07:57:02 -0300

    Projeto criado com spring web

---
+++ src/test/java/com/rc/aplicacaoSpring/AplicacaoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rc.aplicacaoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacaoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
